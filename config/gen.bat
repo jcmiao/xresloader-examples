0f9b44e13077083b87c7efd9301ae7a9c3fc621a
jcmiao <jcmiao@tencent.com>
2021-04-01 12:03:29 +0800

    abc

--- config/gen.bat
+++ config/gen.bat
@@ -2,4 +2,4 @@ copy ..\common\xres-code-generator\template\common\cpp\* ..\server\cfgsrc
 
 py ..\common\xres-code-generator\xrescode-gen.py -i ..\common\xres-code-generator\template -p ..\common\sample.pb -o ..\server\cfgsrc -g ..\common\xres-code-generator\template\config_manager.h.mako -g ..\common\xres-code-generator\template\config_manager.cpp.mako -g ..\common\xres-code-generator\template\config_easy_api.h.mako -g ..\common\xres-code-generator\template\config_easy_api.cpp.mako -l "H:..\common\xres-code-generator\template\config_set.h.mako" -l "S:..\common\xres-code-generator\template\config_set.cpp.mako" -t "server"
 
-py xresconv-cli-1.3.1/xresconv-cli.py -g server sample.xml
+py xresconv-cli-1.3.1/xresconv-cli.py -g server -e server sample.xml
